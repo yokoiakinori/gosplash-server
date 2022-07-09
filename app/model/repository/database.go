6fac42311afab08f7aede99524ed9196f8b664dd
Akinori Yokoi <yokoi_akinori@arsaga.jp>
2022-07-09 20:30:19 +0900

    database接続設定の追加

--- app/model/repository/database.go
+++ app/model/repository/database.go
@@ -3,13 +3,36 @@ package repository
 import (
 	"database/sql"
 	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 var Db *sql.DB
 
 func init() {
+	loadEnv()
+
+	dbUser := os.Getenv("DB_USERNAME")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbAddress := os.Getenv("DB_CONTAINER_ADDRESS")
+	dbName := os.Getenv("DB_DATABASE")
+
 	var err error
 	dataSourceName := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8",
-		"todo-app", "todo-pass"
+		dbUser, dbPassword, dbAddress, dbName
 	)
+
+	Db, err = sql.Open("mysql", dataSourceName)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func loadEnv() {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		fmt.Printf("読み込みに失敗しました。")
+	}
 }
