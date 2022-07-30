0168ea0b43c03dd5583308501d7c3d384a1deeb6
Akinori Yokoi <yokoi_akinori@arsaga.jp>
2022-07-30 20:12:10 +0900

    Iconテーブル作成

---
+++ app/model/icon.go
@@ -0,0 +1,9 @@
+package model
+
+type Icon struct {
+	Id int64 `xorm:"pk autoincr int(64)" form:"id" json:"id"`
+	Path string `xorm:"varchar(60)" form:"path" json:"path"`
+	UserId int64 `xorm:"not null" form:"user_id" json:"user_id"`
+	Created string `xorm:"timestamp created" form:"created" json:"created"`
+	Updated string `xorm:"timestamp updated" form:"updated" json:"updated"`
+}
