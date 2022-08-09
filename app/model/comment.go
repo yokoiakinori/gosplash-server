8dc8f25eafb6a898f1350c1ea87580d32d7bffee
Akinori Yokoi <yokoi_akinori@arsaga.jp>
2022-08-09 23:35:06 +0900

    commentテーブルの作成

---
+++ app/model/comment.go
@@ -0,0 +1,11 @@
+package model
+
+type Comment struct {
+	Id int64 `xorm:"pk autoincr int(64)" form:"id" json:"id"`
+	UserId int64 `xorm:"not null" form:"user_id" json:"user_id"`
+	PostId int64 `xorm:"not null" form:"post_id" json:"post_id"`
+	Content string `xorm:"varchar(255)" form:"content" json:"content"`
+	Created string `xorm:"timestamp created" form:"created" json:"created"`
+	Updated string `xorm:"timestamp updated" form:"updated" json:"updated"`
+	Version int `xorm:"version"`
+}
